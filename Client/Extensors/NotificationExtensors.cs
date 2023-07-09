e9d02bc195d12cff45db6269a6ed479ec040c559
Luis Miguel Jerez Cáceres <luis_jerez1@ucne.edu.do>
2023-07-09 12:56:09 -0400

    Agregando el registro y la notification.

---
+++ Client/Extensors/NotificationExtensors.cs
@@ -0,0 +1,23 @@
+﻿using Radzen;
+
+namespace Parcial2_Luis_Miguel_Jerez.Client.Extensors;
+
+public static class NotificationExtensors
+{
+	public static void ShowNotification(this NotificationService notifier,
+		string titulo,
+		string mensaje,
+		NotificationSeverity severity)
+	{
+		var message = new NotificationMessage
+		{
+			Severity = severity,
+			Summary = titulo,
+			Detail = mensaje,
+			Duration = 3000
+		};
+
+		notifier.Notify(message);
+	}
+}
+
