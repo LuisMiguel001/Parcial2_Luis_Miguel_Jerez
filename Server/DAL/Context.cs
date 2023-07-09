e8fdffb9e7aa8486e207e7c1d6dd6806d790e45a
Luis Miguel Jerez Cáceres <luis_jerez1@ucne.edu.do>
2023-07-09 11:36:49 -0400

    Agregando el contexto.

---
+++ Server/DAL/Context.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Parcial2_Luis_Miguel_Jerez.Shared.Models;
+
+namespace Parcial2_Luis_Miguel_Jerez.Server.DAL;
+
+public class Context : DbContext
+{
+	public Context(DbContextOptions<Context> options) 
+		: base(options) { }
+
+	public DbSet<Productos> Productos { get; set; }	
+}
+
