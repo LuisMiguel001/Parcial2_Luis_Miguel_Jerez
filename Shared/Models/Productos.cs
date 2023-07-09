bc8da814ae49bf89090feeddf955f74c57aa5cd7
Luis Miguel Jerez Cáceres <luis_jerez1@ucne.edu.do>
2023-07-09 11:34:16 -0400

    Agregando el modelo.

---
+++ Shared/Models/Productos.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parcial2_Luis_Miguel_Jerez.Shared.Models;
+
+public class Productos
+{
+	[Key]
+
+	[Required(ErrorMessage ="Debe introducir el productoId")]
+	public int ProductoId { get; set; }
+
+	public DateTime Fecha { get; set; }
+
+	[Required(ErrorMessage = "Debe introducir el concepto")]
+	public string? Concepto { get; set; }
+}
+
